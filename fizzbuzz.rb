96143d92396e6d805cac287f08390bb4e4cc0243
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-29 15:42:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num%3 == 0
+  if num%15 == 0
+    return "FizzBuzz"
+  elsif num%3 == 0
     return "Fizz"
   elsif num%5 == 0
     return "Buzz"
-  elsif num%15 == 0 
-    return "FizzBuzz"
   end
 end
